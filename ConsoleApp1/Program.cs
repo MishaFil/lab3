3378361d874988d2a3db0c418b700f4fc6056a1a
DashaUs <dahausanova31@gmail.com>
2023-05-24 00:34:31 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Lab3git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число а:");
+            Console.WriteLine("Введите число b:");
+            Console.WriteLine("1. Сумма");
+            Console.WriteLine("2. Вычитание");
+            Console.WriteLine("3. Умножение");
+            Console.WriteLine("4. Деление");
+
+            Console.WriteLine("Результат: ");
+            int? c = Convert.ToInt16(Console.ReadLine());
+        }
+    }
+}
