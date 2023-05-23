07c27f63c832b135ca29d5ca638ab0010bb0830d
DashaUs <dahausanova31@gmail.com>
2023-05-24 00:38:12 +0700

    InitB

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,6 +6,7 @@
         {
             Console.WriteLine("Введите число а:");
             Console.WriteLine("Введите число b:");
+            double? b = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("1. Сумма");
             Console.WriteLine("2. Вычитание");
             Console.WriteLine("3. Умножение");
